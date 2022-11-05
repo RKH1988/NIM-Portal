de39b543b9ce914c4240b3ca073d6925b21cd9d5
RKH1988 <regettaspearman@gmail.com>
2022-11-05 14:04:14 -0400

    login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 
