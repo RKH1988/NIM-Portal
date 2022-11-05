a7b4ba14f4373c6384cb58a3ecc02d67969e841a
NicholasZervos <nicholaszervos25@gmail.com>
2022-11-06 00:00:07 +0100

    added utils/auth.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
@@ -22,3 +43,6 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
+ // Call the async function to start the server
+ startApolloServer(typeDefs, resolvers);
