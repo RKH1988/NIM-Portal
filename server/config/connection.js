96f837fb4b175fb8e4d8d3788a6fa0d23eb0f7bc
RKH1988 <regettaspearman@gmail.com>
2022-11-08 20:33:17 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/NIM-portal', 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://RKH1988:<123password>@nim-portal.nsvmufw.mongodb.net/?retryWrites=true&w=majority', 
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
